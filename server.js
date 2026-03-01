6e9b62b77b49d16c285bb51afaf9561e682f261b
Jit-Prajapati <jitprajapati.work5523@gmail.com>
2026-03-01 14:19:21 +0530

    Fixed Railway PORT issue

--- server.js
+++ server.js
@@ -268,7 +268,7 @@ app.get("/webhook", (req, res) => {
 // ===============================
 // 📌 START SERVER
 // ===============================
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT} 🚀`);
